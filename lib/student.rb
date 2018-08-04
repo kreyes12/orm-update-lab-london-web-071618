fb43d3b3f41660d91224f16a57ac8c26ded2bdfe
Karla R <karla.y.reyes12@gmail.com>
2018-08-04 05:54:12 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -13,7 +13,8 @@ class Student
   def self.create_table
     sql = <<-SQL
     CREATE TABLE IF NOT EXISTS students (
-      ID INTEGER PRIMARY KEY
+      id INTEGER PRIMARY KEY
+      name PRIMARY TEXT
       
     
     )
