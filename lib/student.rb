d2d07a01ad85e6c5326c7e8baa477b31dc04bdeb
Karla R <karla.y.reyes12@gmail.com>
2018-08-04 05:56:38 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -39,5 +39,9 @@ class Student
       @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
     end
     
+    def self.create 
+      sql = <<-SQL
+      INSERT INTO students 
+    
 
 end
